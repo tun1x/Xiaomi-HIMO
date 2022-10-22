bc83ea2378086d5995da9cc07c9cd955c7d3d239
tun1x <nikodimiusan@gmail.com>
2022-10-22 11:06:47 +0300

    Footer block made
    Finished site without adaptive

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(function(){
 
-    $('.bike-slider').slick({
+    $('.bike-slider, .slider__items').slick({
         arrows: false,
         dots: true,
         fade: true,
@@ -8,5 +8,11 @@ $(function(){
         autoplaySpeed: 5000
     });
 
+
+    let cords = ['scrollX','scrollY'];
+// Перед закрытием записываем в локалсторадж window.scrollX и window.scrollY как scrollX и scrollY
+window.addEventListener('unload', e => cords.forEach(cord => localStorage[cord] = window[cord]));
+// Прокручиваем страницу к scrollX и scrollY из localStorage (либо 0,0 если там еще ничего нет)
+window.scroll(...cords.map(cord => localStorage[cord]));
 
 });
